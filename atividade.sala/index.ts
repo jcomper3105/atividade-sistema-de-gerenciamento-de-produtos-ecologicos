dab5ebab66a8930fbc50203042a6220ed198144f
jeyceComper <jeycegabriellicomper@gmail.com>
2025-03-19 08:52:53 -0300

    aula1

---
+++ atividade.sala/index.ts
@@ -0,0 +1,25 @@
+let nome:string = "jeyce";
+
+console.log(nome) 
+
+var pi = 3.14;
+
+const idadeOld: number =20;
+
+// para ver o valor da variavel //
+console.log(pi)
+
+//paa uma funçao// //template//
+function saudacao(nome, idade){
+    console.log("eu me chamo" .conect(nome, ""))
+}
+
+//objeto
+var pessoa ={
+    nome: "Alice",
+    idade: 15,
+    cidade: "Salvador",
+    apresentar: function(){
+        return "o meu nome  ".concat(this.nome, " e tenho ").concat(this.idade, " e sou da cidade ").concat(this.cidade)
+    }
+}
