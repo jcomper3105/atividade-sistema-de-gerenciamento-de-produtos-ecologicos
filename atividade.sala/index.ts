56adb0b0acf53b381a4b34c2782164aaf7e99dab
jeyceComper <jeycegabriellicomper@gmail.com>
2025-03-20 09:08:06 -0300

    criando o index

--- atividade.sala/index.ts
+++ atividade.sala/index.ts
@@ -1,25 +1,36 @@
-let nome:string = "jeyce";
+let nuemro: number = 10;
+console.log(nuemro);
 
-console.log(nome) 
+let nome:string = "Jeyce";
+console.log(nome);
 
-var pi = 3.14;
+const pi:number = 3.14;
+console.log(pi);
 
-const idadeOld: number =20;
-
-// para ver o valor da variavel //
-console.log(pi)
-
-//paa uma funçao// //template//
-function saudacao(nome, idade){
-    console.log("eu me chamo" .conect(nome, ""))
+function saudacao(nome:string, idade:number){
+    console.log('olá eu me chamo ${nome} e tenho ${idade} anos');
 }
+
+const nomeUsuario:string = "Jeyce";
+const idade:number = 17;
+
+saudacao(nomeUsuario,idade);
 
 //objeto
 var pessoa ={
     nome: "Alice",
     idade: 15,
     cidade: "Salvador",
-    apresentar: function(){
-        return "o meu nome  ".concat(this.nome, " e tenho ").concat(this.idade, " e sou da cidade ").concat(this.cidade)
+    apresentar: function():string{
+        return 'olá meu nome é ${this.nome}, e tenho ${this.idade} e sou da cidade de ${this.cidade}'
     }
 }
+
+console.log(pessoa.apresentar());
+
+function somar(a: number, b: number): number{
+    return a + b;
+}
+
+console.log(somar(3,4)); //saida:7
+//console.log(somar('3','4'));// Erro de tipo em em tempo de compilação
